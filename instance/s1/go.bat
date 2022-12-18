b65104c31aa72ef67f4d24895fee4845f82a4d22
Laika <111684641+laika-murasaki@users.noreply.github.com>
2022-12-18 09:48:50 +0700

    mp-slave-s1 setup

---
+++ instance/s1/go.bat
@@ -0,0 +1,14 @@
+@echo off
+title Hynse Instance - S1
+
+:RESTART
+
+SET serverjar=MultiPaper-paperclip-1.19.3-R0.1-SNAPSHOT-reobf
+SET jdk=%LIGHT_JDK_18%/bin/java.exe
+SET mem=12288
+SET port=40001
+
+%jdk% -Xms%mem%M -Xmx%mem%M --add-modules=jdk.incubator.vector -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -jar %serverjar%.jar --nogui --port %port%
+
+GOTO RESTART
+pause
